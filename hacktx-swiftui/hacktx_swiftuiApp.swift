bc7d6d35cf6d33a0013c0667d3862eb4866f3f8c
aidenseibel <aiden.seibel@gmail.com>
2023-10-21 19:21:05 -0500

    Initial Commit

---
+++ hacktx-swiftui/hacktx_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hacktx_swiftuiApp.swift
+//  hacktx-swiftui
+//
+//  Created by Aiden Seibel on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct hacktx_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
